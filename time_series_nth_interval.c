7dd85cdc60044383e5f9fc61f25de1f26cd7d303
qmarsun <105653354+qmarsun@users.noreply.github.com>
2023-04-01 18:23:55 -0400

    Update time_series_nth_interval.c

--- time_series_nth_interval.c
+++ time_series_nth_interval.c
@@ -6,7 +6,7 @@ Write your code in this editor and press "Run" button to compile and execute it.
 
 *******************************************************************************/
 /* 
-https: //www.onlinegdb.com/online_c_compiler
+https://www.onlinegdb.com/online_c_compiler
 
 From: Thursday, January 1, 1998 at 12:00:00 midnight
 To: Sunday, January 1, 2023 at 12:00:00 midnight
@@ -39,21 +39,26 @@ Alternative time units
 #define MAX_HOUR 24
 #define MAX_DAY 31  
 #define MAX_MONTH 12
-#define MAX_NUMBER_OF_YEARS 25
+#define MAX_NUMBER_OF_YEARS 23 
+//( 2000 - 20023)
 
 struct DPminute {
     int val;
 } ;
 
 struct DPhour {
-     struct minute *min;
+     struct DPminute *min;
 };
 
 struct DPDay {
     struct DPhour *hour;
 };
 
+int allcate_perminute(struct DPhour *dphour ) {
+    dphour = malloc(sizeof(struct DPminute) * 50);
+}
 
+/*
 int allocate_memory_data_points(struct DPDay *dpday);
 int allocate_memory_data_points(struct DPDay *dpday) {
     dpday->hour = malloc(sizeof(struct DPhour)* MAX_HOUR);
@@ -64,7 +69,8 @@ int allocate_memory_data_points(struct DPDay *dpday) {
         }
     }
     return 0;
-}
+}*/
+
 
 
 
@@ -80,27 +86,79 @@ int print_nth_minute(struct DPminute *min, int nth)
     return 0;
 }
 
+int try2();
+int try2() {
+    struct DPminute *minute;
+    minute = malloc( sizeof( struct DPminute) * 60);
+    for(int i=0; i<= 60; i++) {
+       /* *((minute+i))->val = 10; */
+    }
+}
+
+int try3() {
+    int *dp_hr_min;
+    dp_hr_min = malloc(MAX_NUMBER_OF_YEARS*MAX_MONTH* MAX_DAY*MAX_HOUR *MAX_MINUTE*sizeof(int));
+    
+    for(int y=0; y<=MAX_NUMBER_OF_YEARS; y++) {
+        for(int m=0; m<MAX_MONTH; m++ ) {
+            for(int k=0; k <MAX_DAY; k++) {
+                for(int i=0; i<MAX_HOUR; i++) {
+                    for(int j=0; j <MAX_MINUTE; j++ ) {
+                        int index = y+MAX_MONTH+m+MAX_DAY+k+MAX_HOUR+ i+MAX_MINUTE+ j;
+                        *(dp_hr_min +index) = rand();
+                    } 
+                }
+            }
+        }
+    }
+    //for(int y=0; y<=MAX_NUMBER_OF_YEARS; y++) 
+    int y=10;
+    {
+        for(int m=0; m<MAX_MONTH; m++ ) {
+            for(int k=0; k <MAX_DAY; k++) {
+                for(int i=0; i<MAX_HOUR; i++) {
+                    for(int j=0; j <MAX_MINUTE; j++ ) {
+                        int val =  *(dp_hr_min +y+MAX_MONTH+m+MAX_DAY+k+MAX_HOUR+ i+MAX_MINUTE+ j);
+                        printf("\n value yr=%d mnt=%d, day=%d, hr=%d, min=%d, %d",y,m,k, i,j, val);
+                        
+                    }
+                }
+            }
+        }
+    }
+}
+
 int main()
 {
     //struct hour hour_val[24];
+  // try2();
+  try3();
   
+  /*
     // staring with minute first
-    struct DPminute minute_val[MAX_MINUTE];
+    int *dpminutes;
+    int **dphours;
+    dpminutes = malloc(MAX_MINUTE);
+    dphours = malloc(MAX_HOUR);
+    for(int i=0; i<MAX_HOUR; i++ ) {
+        *(dphours+i) = malloc(MAX_MINUTE);
+        for(int j=0;j < MAX_MINUTE; j++) {
+            *(dphours+i+j) = rand();
+            printf("\n value min %d, %d", i, *(dpminutesi+i+j));
     
-    for(int i=0;i < MAX_MINUTE; i++) {
-        minute_val[i].val = rand();
+        }
     }
+   
+   */
+   
     
-    for(int i=0 ; i < MAX_MINUTE; i++) {
-        printf("\n value min %d, %d", i, minute_val[i].val);
-    }
+   // print_nth_minute(&minute_val,60);
     
-    print_nth_minute(&minute_val,60);
     /*
     print_nth_minute(&minute_val,30);
     print_nth_minute(&minute_val,15);
     print_nth_minute(&minute_val,5);
-    print_nth_minute(&minute_val,1);
+    print_nth_minute(&minute_val,1)
     test
     */
     
